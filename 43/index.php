2cd0d8c4a3e2fdc1a9fc5420a240c6da038b9fc0
dgstranz <dgstranz@gmail.com>
2015-02-10 10:18:32 +0100

    exercise 43 - introducing form

--- 43/index.php
+++ 43/index.php
@@ -6,43 +6,85 @@
 <?php
 require_once('../PHPMailer/class.phpmailer.php');
 require_once('../PHPMailer/PHPMailerAutoload.php');
-include('config.php');
 
-$mail = new PHPMailer();
-$mail->IsSMTP();
-$mail->SMTPDebug = 2;
-$mail->SMTPAuth = true;
-$mail->SMTPSecure = 'ssl';
-$mail->Host = 'smtp.gmail.com';
-$mail->Port = 465;
-$mail->CharSet = 'UTF-8';
-
-$mail->Username = $user;
-$mail->Password = $pass;
-
-// Rellenas aquí los datos
-$mail->AddReplyTo('', 'Servidor');
-$mail->SetFrom('', 'Servidor');
-$mail->AddReplyTo('', 'Servidor');
-
-$mail->AddAddress('', '');
-
-$mail->Subject = 'Prueba de correo electrónico por Sendmail';
-
-$mensaje = 'Když se Řehoř Samsa jednou ráno probudil z nepokojných snů, shledal, že se v posteli proměnil v jakýsi nestvůrný hmyz. Ležel na hřbetě tvrdém jak pancíř, a když trochu nadzvedl hlavu, uviděl své vyklenuté, hnědé břicho rozdělené obloukovitými výztuhami, na jehož vrcholu se sotva ještě držela přikrývka a tak tak že úplně nesklouzla dolů. Jeho četné, vzhledem k ostatnímu objemu žalostně tenké nohy se mu bezmocně komíhaly před očima. Co se to se mnou stalo? pomyslel si. Nebyl to sen. Jeho pokoj, správný, jen trochu příliš malý lidský pokoj, spočíval klidně mezi čtyřmi dobře známými stěnami, Nad stolem, na němž byla rozložena vybalená kolekce vzorků soukenného zboží - Samsa byl obchodní cestující -, visel obrázek, který si nedávno vystřihl z jednoho ilustrovaného časopisu a zasadil do pěkného pozlaceného rámu. Představoval dámu, opatřenou kožešinovou čapkou a kožešinovým boa, jak vzpřímeně sedí a nastavuje divákovi těžký kožešinový rukávník, v němž se jí ztrácí celé předloktí. Řehořův pohled se pak obrátil k oknu a pošmourné počasí - bylo slyšet, jak kapky deště dopadají na okenní plech - ho naplnilo melancholií. Co kdybych si ještě trochu pospal a zapomněl na všechny blázniviny, pomyslel si, to však bylo naprosto neproveditelné, neboť byl zvyklý spát na pravém boku, v nynějším stavu se však do této polohy nemohl dostat. Ať sebou házel sebevětší silou na pravý bok, vždycky se zase zhoupl zpátky naznak. Zkoušel to snad stokrát, zavřel oči, aby se nemusel dívat na zmítající se nohy, a přestal, až když ucítil v boku lehkou, tupou bolest, jakou ještě nikdy nepocítil. Ach bože, pomyslel si, jaké jsem si to vybral namáhavé povolání! Den co den na cestách. Zlobení s prací je mnohem víc než přímo v obchodě doma, a k tomu ještě ten kříž s cestováním, starosti o vlaková spojení, nepravidelné, špatné jídlo, stále se střídající známosti, jež nikdy nenabudou trvalosti, srdečnosti. Aby to všechno čert vzal! Ucítil nahoře na břiše slabé svědění; pomalu se sunul po hřbetě k čelu postele, aby mohl lépe zvednout hlavu; našel svědící místo, poseté spoustou drobných bílých teček, které nedovedl posoudit; a chtěl to místo jednou nohou ohmatat, hned ji však stáhl zpátky, neboť při dotyku ho hrůzou zamrazilo.';
-$mensaje = wordwrap($mensaje, 78, "\r\n");
-
-$mail->MsgHTML($mensaje);
-$mail->AltBody = strip_tags($mensaje);
-
-$cabeceras = 'From: webmaster@example.com' . "\r\n" .
-    'Reply-To: webmaster@example.com' . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
-
-if($mail->Send()) {
-	echo 'Correo enviado';
+if (!isset($_POST['nombre']) || !isset($_POST['email']) || !isset($_POST['mensaje'])) {
+	formulario();
+} elseif (empty(trim($_POST['nombre'])) || empty(trim($_POST['email'])) || empty(trim($_POST['mensaje']))) {
+	echo '<b>Error</b>: debe indicarse como mínimo el nombre, el email y el mensaje que se desea enviar.';
+	formulario();
+} elseif (!preg_match('/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/', $_POST['email'])) {
+	echo '<b>Error</b>: correo electrónico no válido.';
+	formulario();
 } else {
-	echo 'Error: ' . $mail->ErrorInfo;
+	enviar_correo($_POST['nombre'], $_POST['email'], $_POST['mensaje']);
+}
+
+function formulario() {
+	echo '<form action="' . $_SERVER['PHP_SELF'] . '" method="POST">
+	<table>
+		<tr>
+			<td>Nombre</td>
+			<td><input type="text" name="nombre" value="' . (isset($_POST['nombre']) ? $_POST['nombre'] : '') . '"></td>
+		</tr>
+		<tr>
+			<td>Empresa</td>
+			<td><input type="text" name="empresa" value="' . (isset($_POST['empresa']) ? $_POST['empresa'] : '') . '"></td>
+		</tr>
+		<tr>
+			<td>Email</td>
+			<td><input type="email" name="email" value="' . (isset($_POST['email']) ? $_POST['email'] : '') . '"></td>
+		</tr>
+		<tr>
+			<td>Teléfono</td>
+			<td><input type="tel" name="tel" value="' . (isset($_POST['tel']) ? $_POST['tel'] : '') . '"></td>
+		</tr>
+		<tr>
+			<td>Mensaje</td>
+			<td><textarea rows="8" cols="50" name="mensaje">' . (isset($_POST['mensaje']) ? $_POST['mensaje'] : '') . '</textarea></td>
+		</tr>
+		<tr>
+			<td colspan="2"><input type="submit" value="Enviar"></td>
+		</tr>
+	</table>
+	</form>';
+}
+
+function enviar_correo($nombre, $email, $mensaje) {
+	include('config.php');
+	$mail = new PHPMailer();
+	$mail->IsSMTP();
+	$mail->SMTPDebug = 2;
+	$mail->SMTPAuth = true;
+	$mail->SMTPSecure = 'ssl';
+	$mail->Host = 'smtp.gmail.com';
+	$mail->Port = 465;
+	$mail->CharSet = 'UTF-8';
+
+	$mail->Username = $user;
+	$mail->Password = $pass;
+
+	// Rellenas aquí los datos
+	$mail->SetFrom('', 'Servidor');
+	$mail->AddReplyTo($email, $nombre);
+
+	$mail->AddAddress('', '');
+
+	$mail->Subject = 'Prueba de correo electrónico por Sendmail';
+
+	$mensaje = wordwrap($mensaje, 78, "\r\n");
+
+	$mail->MsgHTML($mensaje);
+	$mail->AltBody = strip_tags($mensaje);
+
+	$cabeceras = 'From: webmaster@example.com' . "\r\n" .
+	    'Reply-To: webmaster@example.com' . "\r\n" .
+	    'X-Mailer: PHP/' . phpversion();
+
+	if($mail->Send()) {
+		echo 'Correo enviado';
+	} else {
+		echo 'Error: ' . $mail->ErrorInfo;
+	}
 }
 
 ?>
