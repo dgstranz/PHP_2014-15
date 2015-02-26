d6f77186efd7cc998315a9c5a4773f9a635400d9
dgstranz <dgstranz@gmail.com>
2015-02-26 20:43:12 +0100

    exercise 48

--- 48/index.php
+++ 48/index.php
@@ -7,8 +7,14 @@
 
 <?php
 
-var_dump($_POST);
-form();
+if (!isset($_POST['folder']) || !isset($_POST['ext'])) {
+  form();
+} elseif (empty($_POST['folder']) || !is_dir($_POST['folder']) || empty($_POST['ext'])) {
+  echo 'Deben rellenarse los campos y la ruta de la carpeta debe ser válida.';
+  form();
+} else {
+  results();
+}
 
 function form() {
   echo '<form id="options" action="' . $_SERVER['PHP_SELF'] . '" method="POST">';
@@ -16,7 +22,7 @@ function form() {
   echo '  <table>';
   echo '    <tr>';
   echo '      <td>Carpeta</td>';
-  echo '      <td><input type="file" name="folder" webkitdirectory directory multiple /></td>';
+  echo '      <td><input type="text" name="folder" /></td>';
   echo '    </tr>';
   echo '    <tr>';
   echo '      <td>Extensión</td>';
@@ -29,6 +35,39 @@ function form() {
   echo '</form>';
 }
 
+function results() {
+  $extension = strtolower($_POST['ext']);
+  $handle = opendir($_POST['folder']);
+  $count = 0;
+
+  echo '<h2>Resultados</h2>';
+
+  echo '<ol>';
+  while (($entry = readdir($handle)) !== false) {
+    if (isset(pathinfo($entry)['extension']) && strtolower(pathinfo($entry)['extension']) == $extension) {
+      echo '<li>' . pathinfo($entry)['basename'] . '</li>';
+      $count++;
+    };
+  }
+  echo '</ol>';
+
+  switch (true) {
+    case ($count <= 0):
+      echo 'No se han encontrado archivos';
+      break;
+    
+    case ($count == 1):
+      echo 'Se ha encontrado 1 archivo';
+      break;
+
+    default:
+      echo 'Se han encontrado ' . $count . ' archivos';
+      break;
+  }
+  echo ' con la extensión ' . $_POST['ext'] . ' en la carpeta ' . $_POST['folder'];
+
+  echo '<p><a href="' . $_SERVER['PHP_SELF'] . '">Volver atrás</a></p>';
+}
 
 ?>
 
